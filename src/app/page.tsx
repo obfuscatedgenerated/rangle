0d65cf7a33d350e38ca7c755d02a23d9782736e6
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2026-03-29 20:42:11 +0100

    title margin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
         <main className="flex-1 flex-col m-4 flex items-center justify-center">
             <InfoPopup/>
 
-            <h1 className="font-title text-3xl sm:text-4xl font-bold">Rangle</h1>
+            <h1 className="font-title text-3xl sm:text-4xl mb-1 font-bold">Rangle</h1>
             <Game />
 
             <p className="fixed left-2 bottom-2 opacity-75 text-xs sm:text-base">Powered by <a href="https://www.wikidata.org/" target="_blank" rel="noopener noreferrer" className="underline">Wikidata</a></p>
