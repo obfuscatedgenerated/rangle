50c35713cc98273ef81f3ea044d0804028aa0abe
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2026-03-29 21:20:03 +0100

    fix classes and generate the first daily!

--- bin/generate_daily.js
+++ bin/generate_daily.js
@@ -1,4 +1,17 @@
 const fs = require("fs");
+const { Command } = require("commander");
+
+const program = new Command();
+
+program
+    .name("generate_daily")
+    .description("Generates the daily Rangle puzzle from Wikidata")
+    .option("-d, --difficulty <difficulty>", "Difficulty level (easy, medium, hard)", "random")
+    .option("-n, --neighbourhood <neighbourhood>", "Neighbourhood of values (small, medium, large)", "random");
+
+program.parse();
+
+const { difficulty: difficulty_arg, neighbourhood: neighbourhood_arg } = program.opts();
 
 const EPOCH = new Date("2026-03-29T00:00:00Z");
 const NEIGHBOURHOODS = ["small", "medium", "large"];
@@ -6,34 +19,23 @@ const NEIGHBOURHOODS = ["small", "medium", "large"];
 // all the countable properties to include in the game
 const properties = {
     "small": [
-        { id: "P3150", name: "Maximum Lifespan", suffix: " years", classes: ["Q16521", "Q729"], unit_hint: "in years" }, // Taxon, Animal
-        { id: "P3095", name: "Gestation Period", suffix: " days", classes: ["Q16521", "Q729"], unit_hint: "in days" },
-        { id: "P2050", name: "Wingspan", suffix: " m", classes: ["Q16521", "Q11436"], unit_hint: "in metres" }, // Taxon, Aircraft
+        { id: "P2250", name: "Life Expectancy / Maximum Lifespan", suffix: " years", classes: ["Q729"], unit_hint: "in years" }, // Animal
+        { id: "P3063", name: "Gestation Period", suffix: " days", classes: ["Q729"], unit_hint: "in days" },
+        { id: "P2050", name: "Wingspan", suffix: " m", classes: ["Q729", "Q11436"], unit_hint: "in metres" }, // Animal, Aircraft
         { id: "P2047", name: "Duration / Runtime", suffix: " minutes", classes: ["Q11424", "Q2188189", "Q7889"], unit_hint: "in minutes" }, // Film, Music, Game
-        { id: "P1113", name: "Number of Episodes", suffix: " episodes", classes: ["Q5398426", "Q16521"] }, // TV, Podcast
-        { id: "P1301", name: "Number of Seasons", suffix: " seasons", classes: ["Q5398426"] },
-        { id: "P1536", name: "Alcohol by Volume (ABV)", suffix: "%", classes: ["Q213253", "Q44"], unit_hint: "by percent" }, // Beverage, Beer
-        { id: "P4354", name: "Tempo", suffix: " BPM", classes: ["Q2188189"], unit_hint: "BPM" },
-        { id: "P1128", name: "Number of Floors / Storeys", suffix: " floors", classes: ["Q41176", "Q1021643"] }, // Building, Skyscraper
-        //{ id: "P166", name: "Awards Received", suffix: " awards", classes: ["Q5", "Q43229"], parser: (v) => Array.isArray(v) ? v.length : 1 },
-        // coords are quite heavy for wikidata so disabled for now. could add a filter to force a certain relevance to prevent the scan
-        // {
-        //     id: "P625",
-        //     name: "Distance from Equator",
-        //     suffix: "° from Equator",
-        //     classes: ["Q515", "Q6256"],
-        //     parser: (coord) => {
-        //         const match = coord.match(/Point\([^ ]+\s+([-+]?\d+\.?\d*)\)/);
-        //         return match ? Math.abs(parseFloat(match[1])) : 0;
-        //     }
-        // },
+        { id: "P1113", name: "Number of Episodes", suffix: " episodes", classes: ["Q5398426", "Q20899"] }, // TV, Podcast
+        { id: "P2437", name: "Number of Seasons", suffix: " seasons", classes: ["Q5398426"] },
+        { id: "P2665", name: "Alcohol by Volume (ABV)", suffix: "%", classes: ["Q154", "Q44"], unit_hint: "by percent" }, // Beverage, Beer
+        { id: "P1725", name: "Tempo", suffix: " BPM", classes: ["Q2188189"], unit_hint: "BPM" },
+        { id: "P1101", name: "Number of Floors / Storeys", suffix: " floors", classes: ["Q41176", "Q1021643"] }, // Building, Skyscraper
+        { id: "P1685", name: "Pokédex Number", prefix: "#", suffix: "", classes: ["Q3966183"] }
     ],
 
     "medium": [
         { id: "P2048", name: "Height", suffix: " m", classes: ["Q5", "Q41176", "Q754507", "Q10884"], unit_hint: "in metres" }, // Human, Building, Rocket, Tree
         { id: "P2043", name: "Length", suffix: " m", classes: ["Q4022", "Q12280", "Q11446"], unit_hint: "in metres" }, // River, Bridge, Ship
         { id: "P2044", name: "Elevation above sea level", suffix: " m", classes: ["Q8502", "Q515", "Q1248784"], unit_hint: "in metres" }, // Mountain, City, Airport
-        { id: "P2052", name: "Top Speed", suffix: " km/h", classes: ["Q11436", "Q7946", "Q870", "Q729"], unit_hint: "in km/h" }, // Aircraft, Car, Train, Animal
+        { id: "P2052", name: "Top Speed", suffix: " km/h", classes: ["Q11436", "Q1420", "Q870", "Q729"], unit_hint: "in km/h" }, // Aircraft, Car, Train, Animal
         { id: "P2045", name: "Depth", suffix: " m", classes: ["Q1532", "Q9472"], unit_hint: "in metres" }, // Lake, Ocean
         { id: "P3346", name: "Spiciness (Scoville Scale)", suffix: " SHU", classes: ["Q331469", "Q23146"] }, // Chili, Sauce
         { id: "P1104", name: "Number of Pages", suffix: " pages", classes: ["Q7725", "Q571"] }, // Lit Work, Book
@@ -41,47 +43,50 @@ const properties = {
         { id: "P569", name: "Year of Birth", classes: ["Q5"], parser: (d) => new Date(d).getFullYear() },
         { id: "P570", name: "Year of Death", classes: ["Q5"], parser: (d) => new Date(d).getFullYear() },
         { id: "P575", name: "Year of Discovery", classes: ["Q11344", "Q3863"], parser: (d) => new Date(d).getFullYear() }, // Element, Asteroid
-        { id: "P2102", name: "Boiling Point", suffix: "°C", classes: ["Q11344", "Q11173"], parser: (k) => Math.round(Number(k) - 273.15), unit_hint: "in Celsius" },
+        { id: "P2102", name: "Boiling Point", suffix: "°C", classes: ["Q11344", "Q11173"], parser: (k) => Math.round(Number(k) - 273.15), unit_hint: "in Celsius" }
     ],
 
     "large": [
-        { id: "P1082", name: "Population", suffix: " people", classes: ["Q515", "Q6256"] }, // City, Country
+        { id: "P1082", name: "Population", suffix: " people", classes: ["Q1549591", "Q5119", "Q3624078"] }, // Big City, Capital, Sovereign State
         { id: "P2067", name: "Mass / Weight", suffix: " kg", classes: ["Q634", "Q11022", "Q11446"], unit_hint: "in kilograms" }, // Planet, Star, Ship
         { id: "P2046", name: "Area", suffix: " sq km", classes: ["Q6256", "Q23442", "Q515"], unit_hint: "in sq km" }, // Country, Island, City
-        { id: "P2664", name: "Number of Employees", suffix: " employees", classes: ["Q4830453", "Q2655353"] }, // Business, Agency
+        { id: "P1128", name: "Number of Employees", suffix: " employees", classes: ["Q4830453", "Q2655353"] }, // Business, Agency
         { id: "P1098", name: "Native Speakers", suffix: " speakers", classes: ["Q34770"] },
         { id: "P1110", name: "Event Attendance", suffix: " attendees", classes: ["Q16567", "Q1076531"] }, // Event, Match
         { id: "P3762", name: "Student Enrollment", suffix: " students", classes: ["Q3918", "Q3914"] }, // Uni, School
-        { id: "P2133", name: "Total Revenue", prefix: "$", suffix: "", classes: ["Q4830453", "Q11424"], unit_hint: "in dollars" },
-        { id: "P2403", name: "Total Net Worth", prefix: "$", suffix: "", classes: ["Q5"], unit_hint: "in dollars" },
-        { id: "P2127", name: "Market Capitalisation", prefix: "$", suffix: "", classes: ["Q4830453"], unit_hint: "in dollars" },
+        { id: "P2139", name: "Total Revenue", prefix: "$", suffix: "", classes: ["Q4830453", "Q11424"], unit_hint: "in dollars" },
+        { id: "P2218", name: "Total Net Worth", prefix: "$", suffix: "", classes: ["Q5"], unit_hint: "in dollars" },
+        { id: "P2226", name: "Market Capitalisation", prefix: "$", suffix: "", classes: ["Q4830453"], unit_hint: "in dollars" },
         { id: "P2130", name: "Production Budget", prefix: "$", suffix: "", classes: ["Q11424", "Q7889"], unit_hint: "in dollars" }, // Film, Game
-        { id: "P2139", name: "Total Box Office Revenue", prefix: "$", suffix: "", classes: ["Q11424"], unit_hint: "in dollars" },
-        { id: "P1143", name: "Total Word Count", suffix: " words", classes: ["Q7725", "Q571"] },
-        { id: "P3734", name: "Social Media Followers", suffix: " followers", classes: ["Q5", "Q215380"] }, // Human, Band
-        { id: "P8627", name: "YouTube Subscribers", suffix: " subscribers", classes: ["Q5", "Q4830453"] },
-        { id: "P10985", name: "Spotify Monthly Listeners", suffix: " listeners", classes: ["Q5", "Q215380"] },
-        { id: "P3143", name: "Subreddit Subscribers", suffix: " members", classes: ["Q3962", "Q7889"] }, // Website, Game
+        { id: "P2142", name: "Total Box Office Revenue", prefix: "$", suffix: "", classes: ["Q11424"], unit_hint: "in dollars" },
+        //{ id: "P1143", name: "Total Word Count", suffix: " words", classes: ["Q7725", "Q571"] },
+        //{ id: "P3734", name: "Social Media Followers", suffix: " followers", classes: ["Q5", "Q215380"] }, // Human, Band
+        //{ id: "P8627", name: "YouTube Subscribers", suffix: " subscribers", classes: ["Q5", "Q4830453"] },
+        //{ id: "P10985", name: "Spotify Monthly Listeners", suffix: " listeners", classes: ["Q5", "Q215380"] },
+        //{ id: "P3143", name: "Subreddit Subscribers", suffix: " members", classes: ["Q3962", "Q7889"] }, // Website, Game
         { id: "P1083", name: "Maximum Capacity", suffix: " people", classes: ["Q48310", "Q182832"] }, // Stadium, Hall
         { id: "P2951", name: "Number of Passengers", suffix: " passengers", classes: ["Q1248784", "Q55488"] }, // Airport, Station
-        { id: "P2243", name: "Approximate Distance from Sun", suffix: " km", classes: ["Q634", "Q3863"], unit_hint: "in kilometres" } // Planet, Asteroid
+        { id: "P2243", name: "Approximate Distance from Sun", suffix: " km", classes: ["Q634", "Q3863"], unit_hint: "in kilometres" }, // Planet, Asteroid
+        {
+            id: "P2664",
+            name: "Total Units Sold",
+            suffix: " units",
+            // Classes: Video Game, Album, Book, Product, Product Family, Automobile
+            classes: ["Q7889", "Q2188189", "Q571", "Q2424752", "Q101352", "Q116933"]
+        }
     ]
 };
 
-// TODO: dynamic formatter so market cap doesnt always have to be billion, can write a query to flip between latitude and longitude etc
-
-// horrible stuff
 const exclude_categories = [
     { id: "Q198", name: "War / Armed Conflict" },
     { id: "Q8065", name: "Disaster" },
     { id: "Q12136", name: "Disease / Illness" },
     { id: "Q83267", name: "Crime" },
-    { id: "Q10806174", name: "Terrorist Attack" },
+    { id: "Q128758", name: "Terrorism / Terrorist Attack" },
     { id: "Q13101262", name: "Mass Murder" },
     { id: "Q170584", name: "Serial Killer" },
     { id: "Q3122841", name: "Genocide" },
     { id: "Q170784", name: "Execution" },
-    { id: "Q128758", name: "Terrorist Attack" },
     { id: "Q319965", name: "Massacre" },
     { id: "Q133171", name: "Epidemic / Pandemic" },
     { id: "Q178225", name: "Accident" },
@@ -92,19 +97,30 @@ const exclude_categories = [
     { id: "Q131102", name: "Sex Crime" },
     { id: "Q8050", name: "Slavery" },
     { id: "Q132781", name: "Torture" },
+    { id: "Q728", name: "Weapon" },
+    { id: "Q12774", name: "Explosive" },
+    { id: "Q42811", name: "Sex organ" },
+    { id: "Q184224", name: "Sex toy" },
+    { id: "Q462942", name: "Sex act" }
 ];
+
+const difficulties = {
+    "easy": { label: "Easy", min: 150, max: 3000 },
+    "medium": { label: "Medium", min: 60, max: 149 },
+    "hard": { label: "Hard", min: 15, max: 59 }
+};
 
 const choose_difficulty = () => {
     const rng = Math.random();
 
     // 20% chance for something pretty easy, very famous
-    if (rng < 0.20) return { label: "Easy", min: 45, max: 1000 };
+    if (rng < 0.20) return difficulties.easy;
 
     // 20% chance for something quite hard, niche but verifiable
-    if (rng < 0.40) return { label: "Hard", min: 5, max: 15 };
+    if (rng < 0.40) return difficulties.hard;
 
     // 60% chance for a medium challenge, standard trivia
-    return { label: "Medium", min: 16, max: 44 };
+    return difficulties.medium;
 };
 
 const fetch_single_property = async (prop, difficulty) => {
@@ -216,8 +232,8 @@ const check_if_safe = async (candidates) => {
 
 const main = async () => {
     // pick a difficulty and neighbourhood for today
-    const difficulty = choose_difficulty();
-    const neighbourhood = NEIGHBOURHOODS[Math.floor(Math.random() * 3)];
+    const difficulty = difficulties[difficulty_arg] || choose_difficulty();
+    const neighbourhood = NEIGHBOURHOODS.includes(neighbourhood_arg) ? neighbourhood_arg : NEIGHBOURHOODS[Math.floor(Math.random() * NEIGHBOURHOODS.length)];
 
     console.log(`Today's Rangle will be ${difficulty.label} with ${neighbourhood} values.`);
 
@@ -342,10 +358,10 @@ const main = async () => {
 
         success = true;
 
-        console.log("Today's lineup:");
-        lineup.forEach(item => {
-            console.log(`${item.name} (${item.metric}): ${item.prefix}${item.value}${item.suffix}`);
-        });
+        // console.log("Today's lineup:");
+        // lineup.forEach(item => {
+        //     console.log(`${item.name} (${item.metric}): ${item.prefix}${item.value}${item.suffix}`);
+        // });
 
         // create shuffled lineup to serve to players, but save the original order to check against for the shuffle quality
         const shuffled_lineup = [...lineup].sort(() => 0.5 - Math.random());
@@ -366,6 +382,11 @@ const main = async () => {
                 break;
             }
         }
+
+        console.log("Today's lineup (shuffled and without values to avoid spoilers):");
+        shuffled_lineup.forEach(item => {
+            console.log(`${item.name} (${item.metric})`);
+        });
 
         // build the final data
         const today_date = new Date();
