9e14b73d94e471ed401393e876a879befd3da5ea
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2026-04-03 18:17:28 +0100

    make stats streak care if you played the right day or not

--- src/hooks/useRangleScores.ts
+++ src/hooks/useRangleScores.ts
@@ -7,6 +7,8 @@ import type {SaveStateDay, ScoreState} from "@/hooks/useRangleState";
 export const useRangleScores = () => {
     const [scores, setScores] = useState<ScoreState | null>(null);
 
+    // TODO: recompute scores if game finished
+
     useEffect(() => {
         const migrate_from_state = () => {
             const raw_state = localStorage.getItem("rangle_state_v1");
@@ -29,8 +31,10 @@ export const useRangleScores = () => {
 
                     migrated_scores[date] = {
                         attempts: game_data.attempts.length,
-                        updated: new Date().toISOString(),
-                        result
+                        result,
+
+                        // give the benefit of doubt, set the updated time to when the puzzle was released
+                        updated: new Date(`${date}T12:00:00Z`).toISOString(),
                     };
                 }
 
@@ -73,24 +77,26 @@ export const useRangleScores = () => {
             return null;
         }
 
-        // filter out in progress games and sort by date ascending
         const sorted_entries = Object.entries(scores)
             .filter(([_, score]) => score.result !== undefined)
             .sort(([dateA], [dateB]) => dateA.localeCompare(dateB));
 
         let current_play_streak = 0;
         let longest_play_streak = 0;
-
         let current_win_streak = 0;
         let longest_win_streak = 0;
-
         let wins = 0;
         const score_distribution: Record<number, number> = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 };
 
         let previous_date: Date | null = null;
+        let previous_was_on_time = false;
 
         for (const [date_str, score] of sorted_entries) {
-            // sum wins and score distribution
+            // did they play on the day the puzzle was released? (i.e. not archived play)
+            const played_date_iso = new Date(score.updated).toLocaleDateString("en-CA", { timeZone: time_zone });
+            const played_on_time = played_date_iso === date_str;
+
+            // regardless archived or not, count the win if they got it right
             if (score.result === true) {
                 wins++;
                 if (score.attempts) {
@@ -98,50 +104,47 @@ export const useRangleScores = () => {
                 }
             }
 
-            // utc date maths
+            // utc maths
             const current_date = new Date(`${date_str}T00:00:00Z`);
 
             if (previous_date) {
                 const diff_time = current_date.getTime() - previous_date.getTime();
                 const diff_days = Math.round(diff_time / (1000 * 60 * 60 * 24));
 
-                if (diff_days === 1) {
-                    // next day in a row, so play streak continues
+                if (diff_days === 1 && played_on_time && previous_was_on_time) {
+                    // played on time and next day in a row, so play streak continues
                     current_play_streak++;
 
                     // if they also won, win streak continues, otherwise resets
-                    if (score.result === true) {
-                        current_win_streak++;
-                    } else {
-                        current_win_streak = 0;
-                    }
+                    current_win_streak = score.result === true ? current_win_streak + 1 : 0;
                 } else {
                     // both streaks reset as missed at least one day
-                    current_play_streak = 1;
-                    current_win_streak = score.result === true ? 1 : 0;
+                    current_play_streak = played_on_time ? 1 : 0;
+                    current_win_streak = (played_on_time && score.result === true) ? 1 : 0;
                 }
             } else {
                 //  first entry, so start play streak and win streak if they won
-                current_play_streak = 1;
-                current_win_streak = score.result === true ? 1 : 0;
+                current_play_streak = played_on_time ? 1 : 0;
+                current_win_streak = (played_on_time && score.result === true) ? 1 : 0;
             }
 
             // compare against current longest streaks
-            if (current_play_streak > longest_play_streak) {
+            if (current_play_streak > longest_play_streak){
                 longest_play_streak = current_play_streak;
             }
+
             if (current_win_streak > longest_win_streak) {
                 longest_win_streak = current_win_streak;
             }
 
             previous_date = current_date;
+            previous_was_on_time = played_on_time;
         }
 
         // check if streaks died today
         if (previous_date) {
             const today_iso = new Date().toLocaleDateString("en-CA", { timeZone: time_zone });
             const today_utc = new Date(`${today_iso}T00:00:00Z`);
-
             const days_since_last_play = Math.round((today_utc.getTime() - previous_date.getTime()) / (1000 * 60 * 60 * 24));
 
             // if they haven't played today or yesterday, streaks are dead
