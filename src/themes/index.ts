2cc08c0590dbb79221fc1ecdd2a0f3450c594f3d
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2026-04-04 18:38:53 +0100

    pride theme

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -10,11 +10,17 @@ export interface ThemeDefinition {
 
 import fools from "./fools.module.css";
 import gold from "./gold.module.css";
+import pride from "./pride.module.css";
 
 export const THEMES: {[id: string]: ThemeDefinition} = {
     default: {
         name: "Classic",
         css_class: ""
+    },
+
+    pride: {
+        name: "Pride",
+        css_class: pride.theme,
     },
 
     fools: {
@@ -36,7 +42,7 @@ export const THEMES: {[id: string]: ThemeDefinition} = {
         css_class: gold.theme,
         criteria_description: "solving at least 14 Rangles",
         criteria: ({stats}) => stats.wins >= 14
-    }
+    },
 } as const;
 
 export type ThemeID = keyof typeof THEMES;
