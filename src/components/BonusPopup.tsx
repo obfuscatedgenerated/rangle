9c52a10a2935ccdc94996ebe958766a0f9523ef0
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2026-04-07 20:22:34 +0100

    suspenseful reveal

--- src/components/BonusPopup.tsx
+++ src/components/BonusPopup.tsx
@@ -3,6 +3,7 @@
 import {useCallback, useEffect, useRef, useState} from "react";
 
 import type {PuzzleStat} from "@/components/Game";
+import {SearchSpotlights} from "@/components/SearchSpotlights";
 
 interface BonusPopupProps {
     open: boolean;
@@ -30,6 +31,11 @@ export const BonusPopup = ({open, on_finish, bonus_rounds}: BonusPopupProps) => 
         []
     );
 
+    const [card_classes, setCardClasses] = useState<Record<string, string>>({});
+
+    const [suspenseful, setSuspenseful] = useState(false);
+    const [revealed_answers, setRevealedAnswers] = useState<boolean>(false);
+
     const handle_submit = useCallback(
         () => {
             const results: Record<string, boolean> = {};
@@ -56,48 +62,78 @@ export const BonusPopup = ({open, on_finish, bonus_rounds}: BonusPopupProps) => 
                 results[stat.id] = input_number >= lower_bound && input_number <= upper_bound;
             }
 
-            on_finish(results);
+            setSuspenseful(true);
 
-            // TODO: suspensefully reveal the correct answers with an animation and maybe drumroll
+            setTimeout(() => {
+                setSuspenseful(false);
+
+                // reveal results after the delay, marking the ones that were within the 5% margin as correct and the others as incorrect
+                const new_card_classes: Record<string, string> = {};
+                const new_input_states: Record<string, string> = {};
+
+                for (const stat of bonus_rounds) {
+                    const correct = results[stat.id];
+
+                    new_card_classes[stat.id] = correct ? "border-2 border-green-500" : "border-2 border-red-500";
+                    new_input_states[stat.id] = `${stat.prefix}${stat.value.toLocaleString()}${stat.suffix}`
+                }
+
+                setCardClasses(new_card_classes);
+                setInputStates(new_input_states);
+
+                setRevealedAnswers(true);
+
+                setTimeout(() => {
+                    on_finish(results);
+                }, 3000);
+            }, 3000);
         },
         [bonus_rounds, input_states, on_finish]
     );
 
     return (
-        <dialog ref={dialog_ref} className="rounded-lg p-4 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[95vw] sm:max-w-md w-full bg-background-variant text-foreground-variant flex flex-col items-center">
-            <h2 className="font-title text-xl font-bold">Bonus Round!</h2>
+        <>
+            <SearchSpotlights active={suspenseful} />
 
-            <b className="mt-2">Congratulations on solving today&apos;s Rangle!</b>
-            <p>However, {bonus_rounds.length === 1 ? "one of the" : "some"} values {bonus_rounds.length === 1 ? "has" : "have"}n&apos;t been revealed...</p>
+            <dialog ref={dialog_ref} className="rounded-lg p-4 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[95vw] sm:max-w-md w-full bg-background-variant text-foreground-variant flex flex-col items-center">
 
-            <p className="my-4 text-center text-pretty">Can you guess the missing value{bonus_rounds.length > 1 ? "s" : ""} <b>within 5%</b> to score {bonus_rounds.length === 1 ? "a" : "some"} bonus point{bonus_rounds.length === 1 ? "" : "s"}?</p>
 
-            <div className="flex flex-col items-center mt-4">
-                {bonus_rounds.map((stat, index) => (
-                    <div key={index} className="mb-4 p-4 bg-background rounded w-full">
-                        <p className="text-pretty text-center text-lg sm:text-2xl font-bold">{stat.name}</p>
-                        <p className="uppercase tracking-wider text-pretty text-sm sm:text-base text-center mb-2">{stat.metric}</p>
+                <h2 className="font-title text-xl font-bold">Bonus Round!</h2>
 
-                        <input
-                            className="w-full p-2 border border-gray-300 rounded mb-2 text-center"
-                            type="number"
-                            placeholder={`Your guess${stat.unit_hint ? ` (${stat.unit_hint})` : ""}`}
+                <b className="mt-2">Congratulations on solving today&apos;s Rangle!</b>
+                <p>However, {bonus_rounds.length === 1 ? "one of the" : "some"} values {bonus_rounds.length === 1 ? "has" : "have"}n&apos;t been revealed...</p>
 
-                            value={input_states[stat.id] || ""}
-                            onChange={(e) => dispatch_input_state({ id: stat.id, value: e.target.value })}
-                        />
+                <p className="my-4 text-center text-pretty">Can you guess the missing value{bonus_rounds.length > 1 ? "s" : ""} <b>within 5%</b> to score {bonus_rounds.length === 1 ? "a" : "some"} bonus point{bonus_rounds.length === 1 ? "" : "s"}?</p>
 
-                        <p className="text-pretty text-center text-sm opacity-60">({stat.description})</p>
-                    </div>
-                ))}
-            </div>
+                <div className="flex flex-col items-center mt-4">
+                    {bonus_rounds.map((stat, index) => (
+                        <div key={index} className={`mb-4 p-4 bg-background rounded w-full ${card_classes[stat.id]}`}>
+                            <p className="text-pretty text-center text-lg sm:text-2xl font-bold">{stat.name}</p>
+                            <p className="uppercase tracking-wider text-pretty text-sm sm:text-base text-center mb-2">{stat.metric}</p>
 
-            <button
-                className="w-full text-lg font-bold uppercase tracking-wider mt-4 mb-2 sm:mt-6 sm:mb-4 px-4 py-3 bg-primary text-on-primary rounded cursor-pointer"
-                onClick={handle_submit}
-            >
-                Submit guess{bonus_rounds.length > 1 ? "es" : ""}
-            </button>
-        </dialog>
+                            <input
+                                disabled={suspenseful || revealed_answers}
+
+                                className="w-full p-2 border border-gray-300 rounded mb-2 text-center"
+                                type={revealed_answers ? "text" : "number"}
+                                placeholder={`Your guess${stat.unit_hint ? ` (${stat.unit_hint})` : ""}`}
+
+                                value={input_states[stat.id] || ""}
+                                onChange={(e) => dispatch_input_state({ id: stat.id, value: e.target.value })}
+                            />
+
+                            <p className="text-pretty text-center text-sm opacity-60">({stat.description})</p>
+                        </div>
+                    ))}
+                </div>
+
+                <button
+                    className="w-full text-lg font-bold uppercase tracking-wider mt-4 mb-2 sm:mt-6 sm:mb-4 px-4 py-3 bg-primary text-on-primary rounded cursor-pointer"
+                    onClick={handle_submit}
+                >
+                    Submit guess{bonus_rounds.length > 1 ? "es" : ""}
+                </button>
+            </dialog>
+        </>
     );
 }
