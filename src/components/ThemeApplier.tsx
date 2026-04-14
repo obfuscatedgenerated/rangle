97305cc0a16d745a140c495ed4418d48a23d3346
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2026-04-14 01:55:07 +0100

    fix nextjs added icon

--- src/components/ThemeApplier.tsx
+++ src/components/ThemeApplier.tsx
@@ -17,14 +17,21 @@ export const ThemeApplier = () => {
         const icon_path = theme_data.icon || "/icon.svg";
 
         // find the rel icon link element with an svg href to replace
-        let link_element = document.querySelector('link[rel="icon"][href$=".svg"]') as HTMLLinkElement | null;
+        // if multiple exist, choose the first and delete the rest
+        const link_elements = document.querySelectorAll('link[rel="icon"][href$=".svg"]') as NodeListOf<HTMLLinkElement> | null;
+        let link_element: HTMLLinkElement | null = null;
 
-        // if not found, find any rel icon link element to replace
-        if (!link_element) {
-            link_element = document.querySelector('link[rel="icon"]') as HTMLLinkElement | null;
+        if (link_elements) {
+            for (const el of link_elements) {
+                if (!link_element) {
+                    link_element = el;
+                } else {
+                    el.remove();
+                }
+            }
         }
 
-        // if still not found, create a new link element
+        // if not found, create a new link element
         if (!link_element) {
             link_element = document.createElement("link");
             link_element.rel = "icon";
