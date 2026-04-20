7e3b13a0e856107c8dbc746e0cfd17636639d6f6
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2026-04-20 23:36:51 +0100

    can only rpc when auth acquired

--- src/components/DiscordPresence.tsx
+++ src/components/DiscordPresence.tsx
@@ -4,6 +4,7 @@ import type {DiscordSDK} from "@discord/embedded-app-sdk";
 
 import {get_discord_sdk, in_discord_activity} from "@/util/discord";
 import {useEffect} from "react";
+import {useAuth} from "@/context/AuthContext";
 
 export type Activity = Parameters<DiscordSDK["commands"]["setActivity"]>[0]["activity"];
 
@@ -12,17 +13,19 @@ interface DiscordPresenceProps {
 }
 
 export const DiscordPresence = ({activity}: DiscordPresenceProps) => {
+    const {via_discord_activity} = useAuth();
+
     useEffect(() => {
-        if (!in_discord_activity() || !activity) {
+        if (!via_discord_activity || !activity) {
             return;
         }
 
         get_discord_sdk().then(sdk => {
-            sdk.commands.setActivity({ activity });
-        }).catch(err => {
-            console.error("Failed to set Discord activity", err);
+            sdk.commands.setActivity({ activity }).catch(err => {
+                console.error("Failed to set Discord activity", err);
+            });
         });
-    }, [activity]);
+    }, [activity, via_discord_activity]);
 
     return null;
 }
