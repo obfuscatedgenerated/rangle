4358601094ece1fca1a890d32ad8f897dd6459a0
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2026-04-26 21:11:04 +0100

    testrun url param support

--- src/app/(tooling)/testrun/page.tsx
+++ src/app/(tooling)/testrun/page.tsx
@@ -1,70 +1,22 @@
-"use client";
-
-import {useEffect, useState} from "react";
-
-import {Game, TodayData} from "@/components/Game";
-import {VirtualRangleStateProvider} from "@/context/RangleStateContext";
-import {VirtualSettingsProvider} from "@/context/SettingsContext";
+import {Metadata} from "next";
+import {Suspense} from "react";
 import {LoadingSpinner} from "@/components/LoadingSpinner";
+import {TestRunInteraction} from "@/components/TestRunInteraction";
+import {VirtualSettingsProvider} from "@/context/SettingsContext";
 
-const TestRun = ({ file }: { file: File }) => {
-    const [json_data, setJsonData] = useState<TodayData | null>(null);
-
-    useEffect(() => {
-        const reader = new FileReader();
-
-        reader.onload = () => {
-            if (typeof reader.result === "string") {
-                try {
-                    const parsed_data = JSON.parse(reader.result);
-                    setJsonData(parsed_data);
-                } catch (err) {
-                    console.error("Error parsing JSON:", err);
-                    setJsonData(null);
-                }
-            } else {
-                console.error("Unexpected file reader result type:", typeof reader.result);
-            }
-        };
-
-        reader.onerror = () => {
-            console.error("Error reading file:", reader.error);
-        };
-
-        reader.readAsText(file);
-    }, [file]);
-
-    if (!json_data) {
-        return <LoadingSpinner />;
-    }
-
-    return (
-        <VirtualRangleStateProvider data={json_data}>
-            <Game archive_date={json_data.date} leaderboard_is_open={false} open_leaderboard={() => {}} close_leaderboard={() => {}} />
-        </VirtualRangleStateProvider>
-    );
-};
+export const metadata: Metadata = {
+    title: "Test Run",
+}
 
 export default function TestRunPage() {
-    const [file, setFile] = useState<File | null>(null);
-
     return (
         <VirtualSettingsProvider>
             <main className="flex-1 flex-col m-4 pb-8 sm:pb-0 flex items-center justify-center">
                 <h1 className="font-title text-3xl sm:text-4xl mb-1 mx-4 sm:mx-6 font-bold">Test Run</h1>
 
-                {file ? (
-                    <TestRun file={file} />
-                ) : (
-                    <label className="flex flex-col items-center justify-center mt-8 cursor-pointer rounded-lg border-2 border-dashed border-gray-400 p-6 text-center hover:border-gray-600">
-                        Upload puzzle JSON
-
-                        <input className="hidden" type="file" accept=".json" onChange={(e) => {
-                            const selectedFile = e.target.files?.[0] || null;
-                            setFile(selectedFile);
-                        }}/>
-                    </label>
-                )}
+                <Suspense fallback={<LoadingSpinner className="mt-4" />}>
+                    <TestRunInteraction/>
+                </Suspense>
             </main>
         </VirtualSettingsProvider>
     );
---
+++ src/components/TestRunInteraction.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import {useEffect, useState} from "react";
+
+import {Game, TodayData} from "@/components/Game";
+import {VirtualRangleStateProvider} from "@/context/RangleStateContext";
+import {LoadingSpinner} from "@/components/LoadingSpinner";
+import {useSearchParams} from "next/navigation";
+
+const TestRun = ({json_data}: { json_data: TodayData }) => (
+    <VirtualRangleStateProvider data={json_data}>
+        <Game archive_date={json_data.date} leaderboard_is_open={false} open_leaderboard={() => {
+        }} close_leaderboard={() => {
+        }}/>
+    </VirtualRangleStateProvider>
+);
+
+export const TestRunInteraction = () => {
+    const [json_data, setJsonData] = useState<TodayData | null>(null);
+
+    const search_params = useSearchParams();
+    const [search_params_checked, setSearchParamsChecked] = useState(false);
+
+    const [processing_file, setProcessingFile] = useState(false);
+
+    // try parsing json from url data
+    useEffect(() => {
+        const url_data_b64 = search_params.get("data");
+        if (!url_data_b64) {
+            setSearchParamsChecked(true);
+            return;
+        }
+
+        try {
+            const url_data = atob(url_data_b64);
+            const parsed_data = JSON.parse(url_data);
+            setJsonData(parsed_data);
+        } catch (err) {
+            console.error("Error parsing JSON from URL parameter:", err);
+        }
+
+        setSearchParamsChecked(true);
+    }, [search_params]);
+
+    if (json_data) {
+        return <TestRun json_data={json_data}/>;
+    }
+
+    if (!search_params_checked) {
+        return <LoadingSpinner className="mt-4" />;
+    }
+
+    return (
+        <label
+            className="flex flex-col items-center justify-center mt-8 cursor-pointer rounded-lg border-2 border-dashed border-gray-400 p-6 text-center hover:border-gray-600">
+            Upload puzzle JSON
+            {processing_file && <LoadingSpinner className="mt-4"/>}
+
+            <input className="hidden" type="file" accept=".json" onChange={(e) => {
+                setProcessingFile(true);
+                const file = e.target.files?.[0] || null;
+
+                if (!file) {
+                    setProcessingFile(false);
+                    setJsonData(null);
+                    return;
+                }
+
+                const reader = new FileReader();
+
+                reader.onload = () => {
+                    if (typeof reader.result === "string") {
+                        try {
+                            const parsed_data = JSON.parse(reader.result);
+                            setJsonData(parsed_data);
+
+                            // add the json data to the url without invoking the search params effect
+                            const new_url = new URL(window.location.href);
+                            new_url.searchParams.set("data", btoa(reader.result));
+                            window.history.replaceState({}, "", new_url.toString());
+                        } catch (err) {
+                            console.error("Error parsing JSON:", err);
+                            setJsonData(null);
+                        }
+                    } else {
+                        console.error("Unexpected file reader result type:", typeof reader.result);
+                    }
+
+                    setProcessingFile(false);
+                };
+
+                reader.onerror = () => {
+                    console.error("Error reading file:", reader.error);
+                    setProcessingFile(false);
+                };
+
+                reader.readAsText(file);
+            }}/>
+        </label>
+    );
+}
